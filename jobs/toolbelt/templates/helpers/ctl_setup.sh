5735f6ffb7a0c6b2374b692375047aa811387e3e
Dennis Bell <dennis.j.bell@gmail.com>
2016-04-14 12:15:03 -0700

    Omit packages from PATH and LD_LIBRARY_PATH if busybox was found

--- jobs/toolbelt/templates/helpers/ctl_setup.sh
+++ jobs/toolbelt/templates/helpers/ctl_setup.sh
@@ -29,6 +29,29 @@ redirect_output ${output_label}
 
 export HOME=${HOME:-/home/vcap}
 
+# Setup the PATH and LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-''} # default to empty
+for package_dir in $(ls -d /var/vcap/packages/*)
+do
+  has_busybox=0
+	# Add all packages' /bin & /sbin into $PATH
+  for package_bin_dir in $(ls -d ${package_dir}/*bin)
+  do
+    # Do not add any packages that use busybox, as impacts builtin commands and
+    # is often used for different architecture (via containers)
+    if [ -f ${package_bin_dir}/busybox ]
+    then
+      has_busybox=1
+    else
+      export PATH=${package_bin_dir}:$PATH
+    fi
+  done
+  if [ "$has_busybox" == "0" ]
+  then
+    export LD_LIBRARY_PATH=${package_dir}/lib:$LD_LIBRARY_PATH
+  fi
+done
+
 # Add all packages' /bin & /sbin into $PATH
 for package_bin_dir in $(ls -d /var/vcap/packages/*/*bin | grep -v busybox)
 do
